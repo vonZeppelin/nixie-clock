bf718dc1dba3515d9ae1048b91170c7867fe17d2
Leonid Bogdanov <leonid_bogdanov@mail.ru>
2018-02-05 23:46:32 +1100

    Settings can be read / saved; autoswitch to clocks mode implemented

--- arduino/src/NixieClock.cpp
+++ arduino/src/NixieClock.cpp
@@ -13,79 +13,193 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <DNSServer.h>
 #include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266WebServer.h>
 #include <FS.h>
+#include <Ticker.h>
 
-enum Mode { config, clocks };
-Mode currentMode;
+/*
+ * Describes ESP8266 controller behavior.
+ */
+class BehaviorInterface {
+  public:
+    virtual ~BehaviorInterface() {}
+    virtual void doLoop() = 0;
+};
 
-// config mode services
-DNSServer *dnsServer;
-ESP8266HTTPUpdateServer *updateServer;
-ESP8266WebServer *webServer;
+/*
+ * Stores a behavior instance and delegates doLoop() calls to it.
+ */
+class Context : public BehaviorInterface {
+  public:
+    ~Context() {
+      setBehavior(nullptr);
+    }
 
-void initConfigMode() {
-  currentMode = config;
+    void doLoop() override {
+      behavior->doLoop();
+    }
 
-  SPIFFS.begin();
+    void setBehavior(BehaviorInterface *newBehavior) {
+      delete behavior;
+      behavior = newBehavior;
+    }
 
-  WiFi.mode(WIFI_AP) && WiFi.softAP("NixieClock", "nixieclock");
+  private:
+    BehaviorInterface *behavior;
+};
 
-  dnsServer = new DNSServer();
-  dnsServer->setTTL(300);
-  dnsServer->start(53, "nixieclock", WiFi.softAPIP());
+// is used to switch between behaviors
+Context context;
 
-  webServer = new ESP8266WebServer();
-  webServer->serveStatic("/", SPIFFS, "/index.html", "max-age=86400");
-  webServer->serveStatic("/chota.css", SPIFFS, "/chota.css", "max-age=86400");
-  webServer->serveStatic("/zepto.js", SPIFFS, "/zepto.js", "max-age=86400");
-  // override ESP8266HTTPUpdateServer GET route
-  webServer->on("/update", HTTP_GET, []() {
-    webServer->send(404, "text/plain", "Not found: /update");
-  });
-  webServer->on("/settings", HTTP_POST, []() {
-    webServer->send(200, "text/plain", "OK");
-  });
-  webServer->begin();
+/*
+ * Clocks mode behavior.
+ */
+class ClocksBehavior : public BehaviorInterface {
+  public:
+    ClocksBehavior() {
+       pinMode(BUILTIN_LED, OUTPUT);
+    }
 
-  updateServer = new ESP8266HTTPUpdateServer();
-  updateServer->setup(webServer);
-}
+    void doLoop() override {
+      while (true) {
+        digitalWrite(BUILTIN_LED, LOW);
+        delay(100);
+        digitalWrite(BUILTIN_LED, HIGH);
+        delay(100);
+      }
+    }
+};
 
+/*
+ * Configuration mode behavior.
+ */
+class ConfigBehavior : public BehaviorInterface {
+  public:
+    ConfigBehavior() {
+      if (!WiFi.mode(WIFI_AP)) {
+        return;
+      }
 
-void handleConfigMode() {
-  dnsServer->processNextRequest();
-  webServer->handleClient();
-}
+      uint8_t macAddr[6];
+      WiFi.softAPmacAddress(macAddr);
+      char ssid[16];
+      sprintf(ssid, "NixieClock %02X%02X", macAddr[0], macAddr[1]);
+      if (!WiFi.softAP(ssid, "nixieclock")) {
+        return;
+      }
 
-void destroyConfigMode() {
-  delete updateServer;
+      if (!SPIFFS.begin()) {
+        return;
+      }
 
-  webServer->stop();
-  delete webServer;
+      webServer.addHandler(new BehaviorSwitcher());
+      // overrides ESP8266HTTPUpdateServer GET route, because we have own UI
+      // must be added before calling updateServer.setup()
+      webServer.on("/update", HTTP_GET, [this]() {
+        webServer.send(404, "text/plain", "Not found: /update");
+      });
+      webServer.on("/settings", HTTP_GET, [this]() {
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject &json = jsonBuffer.createObject();
+        File configFile = SPIFFS.open("/config.txt", "r");
+        if (configFile) {
+          String settings[] = {"ssid", "ssid-psk", "server-url", "tz", "tz-override"};
+          for (int i = 0; i < 5 && configFile.available() > 0; ++i) {
+            json[settings[i]] = configFile.readStringUntil('\r');
+            configFile.readStringUntil('\n');
+          }
+          configFile.close();
+        }
+        String jsonStr;
+        json.printTo(jsonStr);
+        webServer.send(200, "application/json", jsonStr);
+      });
+      webServer.on("/settings", HTTP_POST, [this]() {
+        File configFile = SPIFFS.open("/config.txt", "w+");
+        if (configFile) {
+          configFile.println(webServer.arg("ssid"));
+          configFile.println(webServer.arg("ssid-psk"));
+          configFile.println(webServer.arg("server-url"));
+          String tz = webServer.arg("tz");
+          if (tz.length() > 0) {
+              configFile.println(tz);
+              configFile.println(webServer.arg("tz-override"));
+          } else {
+            configFile.println("manual");
+            configFile.println("+00:00");
+          }
+          configFile.close();
 
-  dnsServer->stop();
-  delete dnsServer;
+          webServer.send(200, "text/plain", "OK");
+        } else {
+          webServer.send(500, "text/plain", "Couldn't read config file");
+        }
+      });
+      webServer.serveStatic("/", SPIFFS, "/index.html", "max-age=86400");
+      webServer.serveStatic("/chota.css", SPIFFS, "/chota.css", "max-age=86400");
+      webServer.serveStatic("/zepto.js", SPIFFS, "/zepto.js", "max-age=86400");
 
-  SPIFFS.end();
-}
+      dnsServer.setTTL(300);
+      // it's OK if DNS server can't start - IP should do fine
+      dnsServer.start(53, "nixieclock", WiFi.softAPIP());
+
+      updateServer.setup(&webServer);
+
+      webServer.begin();
+
+      initialized = true;
+    }
+
+    ~ConfigBehavior() {
+      dnsServer.stop();
+      SPIFFS.end();
+    }
+
+    void doLoop() override {
+      if (initialized) {
+        dnsServer.processNextRequest();
+        webServer.handleClient();
+      }
+    }
+
+  private:
+    /*
+     * A RequestHandler that sets current behavior to clocks mode if no client
+     * requested configraion web page for a specified amount of seconds.
+     */
+    class BehaviorSwitcher : public RequestHandler {
+      public:
+        BehaviorSwitcher(int idleFor = 90) {
+          ticker.once(idleFor, []() {
+            context.setBehavior(new ClocksBehavior());
+          });
+        }
+
+        bool canHandle(HTTPMethod method, String uri) override {
+          ticker.detach();
+          return false;
+        }
+
+      private:
+        Ticker ticker;
+    };
+
+    ESP8266WebServer webServer;
+    ESP8266HTTPUpdateServer updateServer;
+    DNSServer dnsServer;
+    bool initialized = false;
+};
 
 void setup()
 {
-  initConfigMode();
+  context.setBehavior(new ConfigBehavior());
 }
 
 void loop()
 {
-  switch (currentMode) {
-    case config:
-      handleConfigMode();
-      break;
-    case clocks:
-      break;
-  }
+  context.doLoop();
 }
--- arduino/src/debug-ui.py
+++ arduino/src/debug-ui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from flask import Flask, redirect, request
+from flask import Flask, jsonify, redirect, request
 
 
 app = Flask(__name__, static_url_path="", static_folder="")
@@ -14,15 +14,26 @@ def root():
 
 @app.route("/update", methods=["POST"])
 def update():
-    response = "Update Error" if len(request.files) == 0 else "Update Success"
-    return response, 200
+    response = "Update Success" if request.files else "Update Error"
+    return (response, 200)
+
+
+@app.route("/settings", methods=["GET"])
+def get_settings():
+    settings = {
+        "ssid": "WiFi SSID",
+        "server-url": "nixie-server.com:8888",
+        "tz": "detect",
+        "tz-override": "+01:00"
+    }
+    return jsonify(settings)
 
 
 @app.route("/settings", methods=["POST"])
-def settings():
+def save_settings():
     print(request.form)
     return ("Success", 200) if request.form['ssid'] else ("Error", 400)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
